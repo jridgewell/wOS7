2a721cd63a26a7490fda2f1d49fd0bd9e2a5b0c4
Justin Ridgewell <justin@ridgewell.name>
2012-01-15 13:53:20 -0500

    Use CGFloat

--- Classes/WOS7ListApp.m
+++ Classes/WOS7ListApp.m
@@ -1,6 +1,6 @@
 #import "WOS7ListApp.h"
 
-#define UIColorFromRGB(rgbValue) [UIColor colorWithRed:((float)((rgbValue & 0xFF0000) >> 16))/255.0 green:((float)((rgbValue & 0xFF00) >> 8))/255.0 blue:((float)(rgbValue & 0xFF))/255.0 alpha:1.0]
+#define UIColorFromRGB(rgbValue) [UIColor colorWithRed:((CGFloat)((rgbValue & 0xFF0000) >> 16))/255 green:((CGFloat)((rgbValue & 0xFF00) >> 8))/255 blue:((CGFloat)(rgbValue & 0xFF))/255 alpha:1]
 
 @implementation WOS7ListApp
 
